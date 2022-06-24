3767b8a7ade250e6d0ca8059b08ffbd10e3c89c8
Barrientos <rominabarrientos2010@gmail.com>
2022-06-24 12:00:23 -0300

    Initial commit

---
+++ app/src/main/kotlin/Peesoa.kt
@@ -0,0 +1,17 @@
+
+class Pessoa {
+    var name: String = "Romina"
+    var cpf: String = "396.445.728.07"
+
+}
+fun main() {
+    val Romina = Pessoa()
+
+    println(Romina.name)
+    println(Romina.cpf)
+
+}
+
+
+
+
