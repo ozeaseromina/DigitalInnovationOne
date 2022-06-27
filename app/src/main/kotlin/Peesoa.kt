58adeda9ada6d4840c35673aa78afb0ce0fae801
Romina Barrientos <rominabarrientos2010@gmail.com>
2022-06-27 10:54:19 -0300

    created person class

--- app/src/main/kotlin/Peesoa.kt
+++ app/src/main/kotlin/Peesoa.kt
@@ -3,6 +3,10 @@ class Pessoa {
     var name: String = "Romina"
     var cpf: String = "396.445.728.07"
 
+
+    Inter class endereco {
+        var rua: String = "Rua teste"
+    }
 }
 fun main() {
     val Romina = Pessoa()
