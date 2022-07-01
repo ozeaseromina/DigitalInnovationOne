1b7f0b342809cda293ee59a4729987d7bf07386d
Romina Barrientos <rominabarrientos2010@gmail.com>
2022-07-01 17:39:34 -0300

    update to talk about class members

--- app/src/main/kotlin/Peesoa.kt
+++ app/src/main/kotlin/Peesoa.kt
@@ -2,18 +2,17 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Jether"
-
     var cpf: String = "123.123.123.11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
-fun main() {
+fun   main() {
     val Jether = Pessoa()
-
-    println(Jether.nome)
-    println(Jether.cpf)
-
-
+    println(Jether.pessoaInfo())
 }
 
 
