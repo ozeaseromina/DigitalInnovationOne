843964b4aa2a9cf19f5580f173b59c7bc9840019
Romina Barrientos <rominabarrientos2010@gmail.com>
2022-07-06 12:05:13 -0300

    commit Digital_Innovation_One

--- app/src/main/kotlin/testes/TesteClienteTipoEnum.kt
+++ app/src/main/kotlin/testes/TesteClienteTipoEnum.kt
@@ -3,4 +3,13 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.ClienteTipo
 
 fun main() {
-    ClienteTipo.values().forEach {  }
+    ClienteTipo.values().forEch { elemento ->
+        println("${elemento.nome} --${elemento} }
+    }
+
+    ClienteTipo.values().forEch { it: ClienteTipo
+        println("${elemento.nome} --${
+}
+
+
+}
