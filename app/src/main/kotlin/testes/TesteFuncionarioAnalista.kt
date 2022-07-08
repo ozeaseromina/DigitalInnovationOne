ccf4cccdf6aeb59bf61f37498e9445c614b8986d
Romina Barrientos <rominabarrientos2010@gmail.com>
2022-07-08 13:09:23 -0300

    created tests and report printer

---
+++ app/src/main/kotlin/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+
+fun main() {
+    val joao = Analista("Joao Pedro", "1234567899", 2000.0)
+    ImprimeRelatorioFuncionario.imprime(joao)
+}
+
+
+
